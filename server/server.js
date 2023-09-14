d361219264e621a5d2f4eddb717f83b505e8d9f3
BaishaliD <baishali95920@gmail.com>
2023-09-14 20:23:58 +0530

    deployment

--- server/server.js
+++ server/server.js
@@ -4,30 +4,30 @@ import cors from "cors";
 import React from "react";
 import { renderToString } from "react-dom/server";
 import path from "path";
-// import Dashboard from "../src/pages/Dashboard/Dashboard.js";
+import Dashboard from "./view/Dashboard.js";
 
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: ["https://deploy-mern-frontend.vercel.app"],
-//     methods: ["POST", "GET"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: ["https://finance-tracker-frontend.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 app.use("/users", userRoutes);
 
 // Serve static files from the 'build' directory for client-side rendering
-app.use(
-  express.static(path.join(__dirname, "../client/build"), { index: false })
-);
+// app.use(
+//   express.static(path.join(__dirname, "../client/build"), { index: false })
+// );
 
 app.get("/healthcheck", function (req, res) {
   res.send("Ok done!");
@@ -36,16 +36,15 @@ app.get("/healthcheck", function (req, res) {
 // Server-side rendering for the dashboard route using EJS template
 app.get("/dashboard/:slug1/:slug2/:slug3", (req, res) => {
   console.log("SLUGS ", req.params.slug1, req.params.slug2, req.params.slug3);
-  // const dashboardHtml = renderToString(
-  //   <div id="root">
-  //     <Dashboard
-  //       income={req.params.slug1}
-  //       expense={req.params.slug2}
-  //       balance={req.params.slug3}
-  //     />
-  //   </div>
-  // );
-  const dashboardHtml = renderToString(<h1>Hello world</h1>);
+  const dashboardHtml = renderToString(
+    <div id="root">
+      <Dashboard
+        income={req.params.slug1}
+        expense={req.params.slug2}
+        balance={req.params.slug3}
+      />
+    </div>
+  );
   return res.send(`
     <html>
       <body>
@@ -58,11 +57,11 @@ app.get("/dashboard/:slug1/:slug2/:slug3", (req, res) => {
 });
 
 // Client-side rendering for other routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+// });
 
-const PORT = process.env.port || 5002;
+const PORT = process.env.port || 5000;
 const mongoDB = `mongodb+srv://baishali95920:MHZH47CjTfmPsCGX@cluster1.fwclc62.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose
